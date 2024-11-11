ac2ef43305db2c9607d8d16fbe1c78006ff063e5
Samuel Cuti <robsamuel.cuti@epn.edu.ec>
2024-11-11 16:22:05 -0500

    Repaso Ionic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,17 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { environment } from 'src/environments/environment';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getStorage, provideStorage } from '@angular/fire/storage';
+
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, provideFirebaseApp(() => initializeApp(environment.firebaseconfig)), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideStorage(() => getStorage())],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,26 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase:{
+    apiKey: "AIzaSyBWUnflxXa_EmrnHkHFzLeDQNTVhDrXdBs",
+    authDomain: "appclase07.firebaseapp.com",
+    projectId: "appclase07",
+    storageBucket: "appclase07.firebasestorage.app",
+    messagingSenderId: "497854217255",
+    appId: "1:497854217255:web:50f51ff28368957e15e202",
+    measurementId: "G-9QZEBD76PE"
+  },
+  firebaseconfig:{
+    apiKey: "AIzaSyDp4RCs8UwhBrTNivTVoBkbDwQ_s1LYxLI",
+    authDomain: "b-1accc.firebaseapp.com",
+    projectId: "b-1accc",
+    storageBucket: "b-1accc.firebasestorage.app",
+    messagingSenderId: "606986248368",
+    appId: "1:606986248368:web:df6c0a16fa5bc4ed9a9b75",
+    measurementId: "G-QDN8GEEH17"
+
+  }
 };
 
 /*
