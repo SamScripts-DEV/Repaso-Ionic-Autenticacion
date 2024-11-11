1fc7f01fcae521bb8c11d047b47684ed65f1523e
Samuel Cuti <robsamuel.cuti@epn.edu.ec>
2024-11-11 17:06:34 -0500

    Config

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,20 +1,27 @@
-import * as dotenv from 'dotenv'
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
-dotenv.config()
 
 export const environment = {
   production: false,
   firebase:{
-    apiKey: process.env.apiKey,
-    authDomain: process.env.authDomain,
-    projectId: process.env.projectId,
-    storageBucket: process.env.storageBucket,
-    messagingSenderId: process.env.messagingSenderId,
-    appId: process.env.appId,
-    measurementId: process.env.measurementId
-    
+    apiKey: "AIzaSyBWUnflxXa_EmrnHkHFzLeDQNTVhDrXdBs",
+    authDomain: "appclase07.firebaseapp.com",
+    projectId: "appclase07",
+    storageBucket: "appclase07.firebasestorage.app",
+    messagingSenderId: "497854217255",
+    appId: "1:497854217255:web:50f51ff28368957e15e202",
+    measurementId: "G-9QZEBD76PE"
+  },
+  firebaseconfig:{
+    apiKey: "AIzaSyDp4RCs8UwhBrTNivTVoBkbDwQ_s1LYxLI",
+    authDomain: "b-1accc.firebaseapp.com",
+    projectId: "b-1accc",
+    storageBucket: "b-1accc.firebasestorage.app",
+    messagingSenderId: "606986248368",
+    appId: "1:606986248368:web:df6c0a16fa5bc4ed9a9b75",
+    measurementId: "G-QDN8GEEH17"
+
   }
 };
 
